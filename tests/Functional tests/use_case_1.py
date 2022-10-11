78316acc8d9396ae8435275e57706350fadeac0a
ChristelleDS <christelle.dasilva75@gmail.com>
2022-10-11 16:43:37 +0200

    work on functional tests

--- tests/Functional tests/use_case_1.py
+++ tests/Functional tests/use_case_1.py
@@ -3,6 +3,19 @@ from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox()
 
+# USE CASE 1:
+# Visit the points board page
+# Login
+# Welcome Page
+# Book a competition with success
+# logout
+
+
+
+# Load the BOARD page
+browser.get('http://127.0.0.1:5000/pointsBoard')
+assert 'Clubs Board' in browser.title
+
 # Load the login page
 browser.get('http://127.0.0.1:5000/')
 assert 'GUDLFT Registration' in browser.title
