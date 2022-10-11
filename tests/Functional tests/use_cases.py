7a0cbc2b7439dfdb57f8046c3fe0fd0f5d31ce47
ChristelleDS <christelle.dasilva75@gmail.com>
2022-10-11 16:33:11 +0200

    work on functional tests

--- tests/Functional tests/use_cases.py
+++ tests/Functional tests/use_cases.py
@@ -7,7 +7,14 @@ browser = webdriver.Firefox()
 browser.get('http://127.0.0.1:5000/')
 assert 'GUDLFT Registration' in browser.title
 
-elem = browser.find_element(By.NAME, 'email')  # Find the email input
+# Find the email input field
+elem = browser.find_element(By.NAME, 'email')
 elem.send_keys('john@simplylift.co')  # write a valid email
+
+# Find the validation button and click on
+button = browser.find_element(By.ID, 'email_validation')
+button.click()
+
+browser
 
 browser.quit()
