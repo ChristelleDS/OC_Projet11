774805c204ad92206c6ad62e0b1d9dcca82ee96b
ChristelleDS <christelle.dasilva75@gmail.com>
2022-10-12 14:27:34 +0200

    BUG_purchase_updatePointsJson

--- server.py
+++ server.py
@@ -3,14 +3,17 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 import datetime
 
 
+clubs_json = 'clubs.json'
+competitions_json = 'competitions.json'
+
 def loadClubs():
-    with open('clubs.json') as c:
+    with open(clubs_json) as c:
          listOfClubs = json.load(c)['clubs']
          return listOfClubs
 
 
 def loadCompetitions():
-    with open('competitions.json') as comps:
+    with open(competitions_json) as comps:
          listOfCompetitions = json.load(comps)['competitions']
          return listOfCompetitions
 
@@ -27,14 +30,50 @@ def index():
     return render_template('index.html')
 
 
-# Function to convert string to datetime
 def convert_strToDate(date_time):
+    """
+    Function to convert string to datetime
+    """
     datetime_str = datetime.datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
     return datetime_str
 
+
 def get_open_competitions(competitions):
+    """
+    Function to return only future competitions
+    :param competitions: competitions list
+    :return: list of future competitions
+    """
     open_comp = [c for c in competitions if convert_strToDate(c['date']) > datetime.datetime.now()]
     return open_comp
+
+
+def update_points(points, bookings, club_index, compet):
+    """
+    Function to update points of the club
+    :param points: updated points number of the club
+    :param bookings: bookings of the club
+    :param club_index: index of the club
+    :param compet: competition name
+    :return:
+    """
+    with open(clubs_json) as f:
+        data = json.load(f)
+        data['clubs'][club_index]['points'] = points
+        data['clubs'][club_index]['bookings'][compet] = bookings
+        json.dump(data, open(clubs_json, 'w'), indent=4)
+
+
+def update_places(places, compet_index):
+    """
+    Function to update available places of the competition
+    :param places: new number of available places
+    :param compet_index: index of the competition
+    """
+    with open(competitions_json) as f:
+        data = json.load(f)
+        data['competitions'][compet_index]['numberOfPlaces'] = places
+        json.dump(data, open(competitions_json, 'w'), indent=4)
 
 
 @app.route('/showSummary',methods=['POST'])
@@ -61,7 +100,7 @@ def book(competition,club):
     if foundClub and foundCompetition:
         return render_template('booking.html',club=foundClub,competition=foundCompetition)
     else:
-        flash("Something went wrong-please try again")
+        flash("Something went wrong - invalid club or competition")
         return render_template('welcome.html', club=club, competitions=competitions)
 
 
@@ -72,10 +111,11 @@ def purchasePlaces():
     placesRequired = int(request.form['places'])
     placesAvailable = int(competition['numberOfPlaces'])
     club_points = int(club['points'])
+    competitions_futures = get_open_competitions(competitions)
     # UC: tournoi complet
     if placesAvailable <= 0:
         flash('Sorry, complete tournament!')
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=competitions_futures)
     # Nombre de place demandé >= 0 et < 12
     # et < au nombre de places disponibles
     elif placesRequired <= 0 or placesRequired > 12\
@@ -90,8 +130,15 @@ def purchasePlaces():
     else:
         competition['numberOfPlaces'] = placesAvailable-placesRequired
         club['points'] = club_points-placesRequired
+        booking_saving = int(club['bookings'][competition['name']]) + placesRequired
+        update_places(places=str(competition['numberOfPlaces']),
+                      compet_index=competitions.index(competition))
+        update_points(points=str(club['points']),
+                      bookings=str(booking_saving),
+                      club_index=clubs.index(club),
+                      compet=competition['name'])
         flash('Great-booking complete!')
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=competitions_futures)
 
 
 @app.route('/pointsBoard')
